84afbddeed701632a7c0d57a8dddb5b84420be46
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2024-11-29 20:26:36 -0300

    finish 1

--- react-advanced/challenge-1/src/App.jsx
+++ react-advanced/challenge-1/src/App.jsx
@@ -2,22 +2,21 @@ import AnimatedComponent from './components/AnimatedComponent';
 import Calculator from './components/Calculator';
 import ExpensiveCalculationComponent from './components/ExpensiveCalculationComponent';
 import Navbar from './components/Navbar';
+import { ThemeProvider} from './context/ThemeContext';
 
 // import { useTheme, ThemeContext } from './context/ThemeContext';
 
 import './App.css';
 
 function App() {
-    // const { theme, toggleTheme } = useTheme();
-
-    return (
-        <>
-            <Navbar />
-            <ExpensiveCalculationComponent />
-            <Calculator />
-            <AnimatedComponent />
-        </>
-    );
+  return (
+    <ThemeProvider>
+      <div>
+        <Navbar />
+        {/* <MainContent /> */}
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- react-advanced/challenge-1/src/context/ThemeContext.jsx
+++ react-advanced/challenge-1/src/context/ThemeContext.jsx
@@ -1,14 +1,27 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useContext} from 'react';
 
-export const ThemeContext = createContext(null);
+export const ThemeContext = createContext(null); // Creamos el contexto
 
-export const useTheme = () => {
-    const [theme, setTheme] = useState(false);
 
-    const toggleTheme = () => {
-        setTheme((prev) => !prev);
-        document.body.classList.toggle('dark');
+// Crear un custom hook para acceder al contexto
+export const useTheme = () => { // Crear un custom hook para acceder al contexto
+  return useContext(ThemeContext); // Usar el hook useContext para acceder al contexto
+};
+
+
+// crear el proveedor del contexto y envolver la aplicacion
+export const ThemeProvider = ({ children }) => { // crear el proveedor del contexto y envolver la aplicacion
+  const [theme, setTheme] = useState(false); // false = light, true = dark
+
+    const toggleTheme = () => { // funcion para alternar el tema
+        setTheme((prev) => !prev); // Alternar entre true (dark) y false (light)
+        document.body.classList.toggle('dark'); // Añadir o quitar la clase 'dark' al body
     };
 
-    return { theme, toggleTheme };
-};
+
+    return ( // Devolver el proveedor del contexto  y envolver la aplicacion
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        {children}
+      </ThemeContext.Provider>
+    );
+  };
